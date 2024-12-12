3bbf65a079146397ded628bb7b1943daf8f41f3a
Arnab Mondal <arnabmondal604@gmail.com>
2024-12-12 11:22:04 +0530

    configuration added

--- enduserapp/src/main/java/com/user/KafkaConfig.java
+++ enduserapp/src/main/java/com/user/KafkaConfig.java
@@ -1,4 +1,14 @@
 package com.user;
 
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
 public class KafkaConfig {
+
+    @KafkaListener(topics = AppConstant.LOCATION_UPDATE_TOPIC, groupId = AppConstant.GROUP_ID)
+    public void updateLocation(String value){
+
+    }
 }
